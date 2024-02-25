71f8097bf1ed0507a1a7a62b3f10fe37eb68d473
llcain <llcain2012@gmail.com>
2024-02-25 15:37:14 -0600

    Php variables

--- index.php
+++ index.php
@@ -10,8 +10,17 @@
 
     <h1>PHP Hello World running on the web server!</h1>
     <p>This is a paragraph!</p>
+    <p>PHP variables</p>
+
+    
     <?php
+        $x = 5;
+        $y = "Jon";
         echo "Hello World LaQuisha - PHP";
+        echo "<br>";
+        echo $x;
+        echo "<br>";
+        echo $y;
     ?>
 </body>
 </html>
