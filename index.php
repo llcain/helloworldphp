5eaad0dd52c60f89763ae4ea164218c9b2ff166d
llcain <llcain2012@gmail.com>
2024-02-25 15:04:09 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World Program</title>
+</head>
+
+<body>
+
+    <h1>PHP Hello World running on the web server!</h1>
+    <p>This is a paragraph.</p>
+    <?php
+        echo "Hello World LaQuisha - PHP";
+    ?>
+</body>
+</html>
