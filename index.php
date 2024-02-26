5851afe9a352d84eb2bcb360755d9d9fcc1f5fd2
llcain <llcain2012@gmail.com>
2024-02-25 19:55:26 -0600

    Updated  php file

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Hello World Program</title>
     <link rel="stylesheet" href="styles.scss">
 </head>
@@ -12,6 +13,8 @@
     <h1>PHP Hello World running on the web server!</h1>
     <p>This is a paragraph!</p>
     <p>PHP variables</p>
+
+    <!-- <div id="mydiv"></div> -->
 
     
     <?php
@@ -23,5 +26,21 @@
         echo "<br>";
         echo $y;
     ?>
+
+    <!-- <script type="text/babel">
+
+        function Hello () {
+            return <h1>What!! What!</h1>;
+        }
+
+        const container = document.getElementById('mydiv');
+        const root = ReactDOM.createRoot(container);
+        root.render(<Hello />)
+    </script>
+    <script scr="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script> -->
+    
+
+    
 </body>
 </html>
