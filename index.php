026e75665b384ac974e516b0e3c8a38de006029a
llcain <llcain2012@gmail.com>
2024-02-25 21:00:33 -0600

    Created CSS file

--- index.php
+++ index.php
@@ -5,14 +5,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>Hello World Program</title>
-    <link rel="stylesheet" href="styles.scss">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
 
-    <h1>PHP Hello World running on the web server!</h1>
-    <p>This is a paragraph!</p>
-    <p>PHP variables</p>
+    <h2>Responsive Form</h2>
+    <p>Resize the browser window to see the effect. When the screen is less than 600px wide, make the two columns stack on top of each other.</p>
+
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-25">
+                    <label for="fname">First Name</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" id="fname" name="firstname" placeholder="Your name...">
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-25">
+                    <label for="fname">Last Name</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" id="lname" name="lastname" placeholder="Your last name...">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="country">Country</label>
+                </div>
+                <div class="col-75">
+                    <select name="country" id="country">
+                        <option value="australia">Australia</option>
+                        <option value="canada">Canada</option>
+                        <option value="usa">USA</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="subject">Subject</label>
+                </div>
+                <div class="col-75">
+                    <textarea name="subject" id="subject" style="height: 200px;" placeholder="Write something..."></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
 
     <!-- <div id="mydiv"></div> -->
 
